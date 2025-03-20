686a87f1a5c517b46945d516394ab75f933262db
Blank Evgeniy <blank.eugeny@yandex.ru>
2025-03-20 20:58:46 +0200

    added users types

--- src/entities/users/model/types/type.ts
+++ src/entities/users/model/types/type.ts
@@ -1,1 +1,35 @@
 export type SortType = "alphabetical" | "birthday";
+
+export interface UsersResponse {
+  items: User[];
+}
+
+export interface User {
+  id: string;
+  avatarUrl: string;
+  firstName: string;
+  lastName: string;
+  userTag: string;
+  department: Department;
+  position: string;
+  birthday: string;
+  phone: string;
+}
+
+export const Department = {
+  all: "all",
+  android: "android",
+  ios: "ios",
+  design: "design",
+  management: "management",
+  analytics: "analytics",
+  qa: "qa",
+  back_office: "back_office",
+  frontend: "frontend",
+  hr: "hr",
+  pr: "pr",
+  backend: "backend",
+  support: "support",
+} as const;
+
+export type Department = (typeof Department)[keyof typeof Department];
