285f69100fa181bfa3830c5c4bba31e39ff07f2d
Blank Evgeniy <blank.eugeny@yandex.ru>
2025-03-20 17:23:44 +0200

    redux store setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
 
-import App from "./app/App";
+import { store } from "@/app/store/store";
+import App from "@/app/App";
 
 import "@/app/styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
