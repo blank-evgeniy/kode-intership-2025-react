dc0c66c5c9c7a9ee9d94e25d7b864d41fc03eacf
Blank Evgeniy <blank.eugeny@yandex.ru>
2025-03-20 17:18:39 +0200

    general styles setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router";
 
 import App from "./app/App";
 
+import "@/app/styles/index.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
